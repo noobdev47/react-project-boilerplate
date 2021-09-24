597a93b4ac997117238a3a6cb15573c8adcf7c3d
noobdev47 <therealguy79@yahoo.com>
2021-09-25 00:26:31 +0500

    App.js Route changes implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { Switch } from 'react-router-dom'
 import './App.css'
+import { protectedRoutes, publicRoutes } from './routes'
+import RouteMiddleware from './routes/middleware'
 
 const App = () => {
   return (
-    <></>
+    <>
+      <Switch>
+        {publicRoutes.map((route, idx) =>
+          < RouteMiddleware
+            key={idx}
+            layout={noAuth}
+            path={route.path}
+            isAuthProtected={false}
+            component={route.component}
+          />
+        )}
+
+        {protectedRoutes.map((route, idx) =>
+          < RouteMiddleware
+            key={idx}
+            layout={vertical}
+            path={route.path}
+            isAuthProtected={false}
+            component={route.component}
+          />
+        )}
+      </Switch>
+    </>
   )
 }
 
